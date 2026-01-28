f1f9bd965ca00c085920868ceb9f793e52310643
CodeJack <pawan.mamoria92@gmail.com>
2026-01-29 03:50:04 +0530

    update main

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ app = FastAPI(title="InboxAI API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://inboxai.space",
+        "https://www.inboxai.space"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
