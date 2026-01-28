a864c11d66847deb8dd746b6f1fd07f121acc2bb
CodeJack <pawan.mamoria92@gmail.com>
2026-01-29 03:31:27 +0530

    update main

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-import os
 from fastapi import FastAPI
-from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 from analyzer.score_engine import analyze_email
 from ai.rewrite import rewrite_email
-from openai import OpenAI
 
+app = FastAPI(title="InboxAI API")
 
-load_dotenv()  # loads .env
-
-api_key = os.getenv("OPENAI_API_KEY")
-if not api_key:
-    raise RuntimeError("OPENAI_API_KEY not set")
-
-client = OpenAI(api_key=api_key)
-
-app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
